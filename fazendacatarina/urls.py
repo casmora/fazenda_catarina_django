e5fbc3f22d077b1a535ab0887bce7c9d4584313e
ANTONIO CAS MORA <acstano@yahoo.com.br>
2022-02-03 16:20:18 -0300

    Update urls.py

--- fazendacatarina/urls.py
+++ fazendacatarina/urls.py
@@ -7,6 +7,7 @@ Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
+
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
@@ -19,7 +20,9 @@ from django.urls import path
 from core import views
 
 urlpatterns = [
+  
     path('admin/', admin.site.urls),
     path('teste/', views.teste),
     path('pag2/', views.teste2)
+    path('/', include(router.urls))
 ]
