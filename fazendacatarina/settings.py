d60aa55704bbae7d903c3531a90e2f8870731801
ANTONIO CAS MORA <acstano@yahoo.com.br>
2022-02-08 16:28:25 -0300

    Update settings.py

--- fazendacatarina/settings.py
+++ fazendacatarina/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = ['*',
    'http://fazenda-catarina.herokuapp.com/',
    'https://fazenda-catarina.herokuapp.com/',
    'fazenda-catarina.herokuapp.com',
-    'fazenda-catarina.herokuapp.com/',
+   'fazenda-catarina.herokuapp.com/',
    '127.0.0.1',]
 
 
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'rest_framework',
     
 ]
 
